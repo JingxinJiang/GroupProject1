cf3b7f884210b74ab7276bf1bde1fa366d27a9f8
Brian <Brian@192.168.0.82>
2022-02-11 22:46:14 -0500

    Brittany branch added.

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -1,7 +1,8 @@
 /**
  * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
+ * 
+ * 
+ * Brittany branch added.
  */
 package ca.sheridancollege.project;
 
